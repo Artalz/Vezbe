1a256766028e53f73631efc20e6b6e02a37f63e2
Artalz <62069742+Artalz@users.noreply.github.com>
2020-04-16 23:20:59 +0200

    Add files via upload

---
+++ Funkcije/Funkcije.js
@@ -0,0 +1,59 @@
+const knjige = require("../");
+
+function sveKnjige() {
+	console.log("Sve knjige");
+	console.log(knjige);
+}
+
+function addKnjiga(id,naziv,autor) {
+    knjige.knjige.push({id,naziv,autor});
+    console.log(knjige);
+}
+
+
+function getKnjiga(id){
+knjige.Knjige.forEach(element => {
+	if(element.id == id)
+		console.log(element);
+	}
+}
+
+
+function setAutor(naziv, autor){
+knjige.Knjige.forEach(element => {
+	if(element.naziv == naziv)
+		element.autor = autor
+	}
+	console.log(knjige);
+}
+
+
+
+function obrisi(id){
+console.log("Obrisano:");
+knjige.Knjige.forEach(element => {
+	if(element.id == id)
+		knjige.knjige.splice(id,1);
+	}
+	console.log(knjige);
+}
+
+
+
+
+function getKnjigaByAuthor(autor){
+console.log("Sve knjige tog autora");
+knjige.Knjige.forEach(element => {
+	if(element.autor == autor)
+		console.log(element);
+	}
+	
+}
+
+
+module.exports.sveKnjige = sveKnjige;
+module.exports.addKnjiga = addKnjiga;
+module.exports.getKnjiga = getKnjiga;
+module.exports.setAutor = setAutor;
+module.exports.obrisi = obrisi;
+module.exports.getKnjigaByAuthor = getKnjigaByAuthor;
---
+++ Main/main.js
@@ -0,0 +1,8 @@
+const funkcije = require("../Funkcije/Funkcije");
+
+funkcije.sveKnjige();
+funkcije.addKnjiga(5,"Naziv5","Autor5");
+funkcije.getKnjiga(3);
+funkcije.setAutor("Knjiga1", "PrvaKnjiga");
+funkcije.getKnjigaByAuthor("PrvaKnjiga");
+funkcije.obrisi(2);
